a2a24a6394e2a540a35db1b7e357c1e462146a74
Kalie Ching <kaliefaith@gmail.com>
2026-04-30 23:47:22 -0400

    Add split and merge function skeleton

--- mha_from_aiayn.py
+++ mha_from_aiayn.py
@@ -3,3 +3,20 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class MultiHeadAttention():
+    def __init__(self, d_model, num_heads):
+        # assert that d_model is divisible by number of heads
+        assert d_model % num_heads == 0
+
+        self.num_heads = num_heads
+        self.d_k = d_model // num_heads
+        
+        # projection matrices for queries, keys, and values
+        self.q_proj = nn.Linear(d_model, d_model)
+        self.k_proj = nn.Linear(d_model, d_model)
+        self.v_proj = nn.Linear(d_model, d_model)
+
+    def _split(self, x):
+        NotImplementedError("This method will be implemented to split the data into multiple heads")
+
+    def _merge(self, x):
+        NotImplementedError("This method will be implemented to merge the data from multiple heads back into original shape")
